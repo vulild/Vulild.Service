8d54fa40047267093fb2982d9a61df06349dc691
vulild <vulild@hotmail.com>
2020-05-20 08:41:50 +0800

    添加服务实现

---
+++ src/Vulild.Service.Log/ILogService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vulild.Service.Log
+{
+    public interface ILogService : IService
+    {
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="log"></param>
+        void WriteLog(string log);
+    }
+}
