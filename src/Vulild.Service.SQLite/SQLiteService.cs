aa72bf48b14b0a1d9c14cf887e58292048f09a7c
vulild <vulild@hotmail.com>
2020-11-04 14:45:02 +0800

    1

--- src/Vulild.Service.SQLite/SQLiteService.cs
+++ src/Vulild.Service.SQLite/SQLiteService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using Vulild.Service.Attributes;
 using Vulild.Service.DataBase;
 
 namespace Vulild.Service.SQLite
 {
+    [ServiceOption(Type = typeof(SQLiteServiceOption))]
     public class SQLiteService : DataBaseService
     {
         public override IDbDataParameter GetParameter(KeyValuePair<string, object> value)
